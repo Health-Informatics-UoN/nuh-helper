0e05aaba34956ff10672854dd703a6488eabac4a
Peter LaValle <peter.lavalle@nhs.net>
2026-08-26 11:25:00 +0100

    test case for crashing on unknown patients

---
+++ tests/test_missing_id.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import pytest
+
+from nuh_helper import shift_excel_dates_inplace
+
+
+def test_missing_id(tmp_path: Path) -> None:
+
+    sheet_configs = {
+        "patients": {
+            "patient_id_col": "ptid",
+            "header_row": 1,
+            "skip_rows_after_header": [3],
+            "date_columns": ["dob"],
+        },
+        "deceased": {
+            "patient_id_col": "patient_id",
+            "header_row": 2,
+            "skip_rows_after_header": [4, 5],
+            "date_columns": ["dob"],
+        },
+    }
+
+    source_file = str(Path(__file__).parent / "data/missing-id.xlsx")
+    output_path = tmp_path / "target.xlsx"
+    linking_table_old = tmp_path / "linking_table_old.csv"
+    linking_table_out = tmp_path / "linking_table_out.csv"
+
+    if True:
+        shift_excel_dates_inplace(
+            input_file=str(source_file),
+            output_file=str(output_path),
+            patient_sheet="patients",
+            patient_id_col="ptid",
+            sheet_configs=sheet_configs,
+            min_shift_days=-20,
+            max_shift_days=-1,
+            seed=14333,
+            linking_table_path=str(linking_table_old),
+            linking_table_output=str(linking_table_out),
+        )
+
+        pytest.fail("the above code should generate an exception")
