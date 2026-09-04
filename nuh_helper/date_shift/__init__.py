6938be38842ec5a3df63c45d16432036786dcdc1
Peter LaValle <peter.lavalle@gmail.com>
2026-09-04 13:00:50 +0100

    feat: prevent unknown users (#126)
    Co-authored-by: Peter LaValle <peter.lavalle@nhs.net>

--- nuh_helper/date_shift/__init__.py
+++ nuh_helper/date_shift/__init__.py
@@ -20,6 +20,13 @@ from nuh_helper.date_shift import _excel, _parse, mappings
 logger = logging.getLogger(__name__)
 
 
+class UnknownPatient(Exception):
+    def __init__(self, page: str, id: str) -> None:
+        message = f"Unknown {id=} on {page=}"
+        super().__init__(message)
+        self._message = message
+
+
 def _get_patient_ids_and_shift_mappings(
     input_file: str,
     patient_sheet: str,
@@ -477,7 +484,14 @@ def shift_excel_dates_inplace(
 
             pid_cell = ws.cell(row=row_idx, column=pid_col_idx)
             pid = _parse._normalize_patient_id(pid_cell.value)
-            shift_days = shift_dict.get(pid) if pid is not None else None
+
+            if pid is None:
+                # skip rows with no person id
+                continue
+
+            shift_days = shift_dict.get(pid)
+            if shift_days is None:
+                raise UnknownPatient(sheet_name, pid)
 
             for col_name, date_col_idx in date_col_indices.items():
                 cell = ws.cell(row=row_idx, column=date_col_idx)
@@ -489,9 +503,6 @@ def shift_excel_dates_inplace(
                         original_value, datetime | date
                     ):
                         cell.value = None
-                    continue
-
-                if shift_days is None:
                     continue
 
                 exc_dates = parsed_exceptions.get(col_name, set())
@@ -526,4 +537,5 @@ __all__ = [
     "apply_date_shifts",
     "generate_shift_mappings",
     "load_shift_mappings",
+    UnknownPatient,
 ]
